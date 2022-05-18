793e94579e089df6e0e714cb47b81ec956c16f6a
eduardo-penna <eduardo.penna@kellogg.northwestern.edu>
2022-05-18 04:18:16 +0000

    controllers setup

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -3,4 +3,18 @@ class PlacesController < ApplicationController
         @places = Place.all 
     end 
 
+    def show
+        @place = Place.find(params["id"]) 
+    end
+    
+    def new 
+        @place = Place.new
+    end
+
+    def create
+        @place = Place.new(params["place"])
+        @place.save
+        redirect_to "/places"
+    end 
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources "places"
   resources "posts"
-  #get("/", { :controller => "places", :action => "index" })
+  get("/", { :controller => "places", :action => "index" })
 end
